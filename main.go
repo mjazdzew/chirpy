fdbaceb35600069927840b91a1bc63f44019a57b
mjazdzew <michal.jazdzewski94@gmail.com>
2026-06-22 16:14:56 +0200

    added a custom handler function

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 )
 
+func handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+	w.WriteHeader(200)
+}
+
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handle)
 	srvr := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
